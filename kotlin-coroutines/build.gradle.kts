c547ba754b8a0933a47efe0bc42e5a485bf1142c
stefan <stefan_kreidel@gmx.de>
2023-10-03 14:40:20 +0200

    initial commit

---
+++ kotlin-coroutines/build.gradle.kts
@@ -0,0 +1,24 @@
+plugins {
+    kotlin("jvm") version "1.9.0"
+    application
+}
+
+group = "io.stefankreidel"
+version = "1.0.0"
+
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+kotlin {
+    jvmToolchain(17)
+}
+
+application {
+    mainClass.set("MainKt")
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,22 @@
+rootProject.name = "stefankreidel-example-projects"
+
+include("kotlin-coroutines")
+
+pluginManagement {
+    repositories {
+        mavenLocal()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        mavenLocal()
+        mavenCentral()
+    }
+}
+
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.5.0"
+}
